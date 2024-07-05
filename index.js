99c03b5477bdb89c4ef878215cdd91c5f8a72a7d
KshitizSharma11 <kshitizsharma.00@gmail.com>
2024-07-05 13:10:39 +0530

    basic chat app fulfilled

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const http = require('http');
 const path = require('path');
 const app = express();
 const server = http.createServer(app);
-
+const {Server} = require("socket.io");
 app.use(express.static(path.resolve('./public')));
+
+const io = new Server(server);
+
+io.on('connection', (socket)=>{
+    socket.on('message',(msg)=>{
+        io.emit('message',msg);
+    });
+    console.log('a new user connected : ', socket.id)
+
+});
+
 app.get('/', async function (req, res) {
     res.sendFile('/public/index.html');
 });
