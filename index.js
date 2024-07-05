a1f006b1dd2b284869394eeb6b76305b0f74279d
KshitizSharma11 <kshitizsharma.00@gmail.com>
2024-07-05 11:44:29 +0530

    created basic fronted

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const app = express();
 const server = http.createServer(app);
-async function init(){
-   
-    
-    server.listen(9000,()=>{
-        console.log('listening on port 4000');
-    });
-}
+
+app.use(express.static(path.resolve('./public')));
+app.get('/', async function (req, res) {
+    res.sendFile('/public/index.html');
+});
+server.listen(9000,()=>{
+    console.log('listening on port 9000');
+});
